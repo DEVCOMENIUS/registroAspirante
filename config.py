00f1a72d1379cec4261d7a32cd4864ad991f1520
fernandaAgustin <al222310475@gmail.com>
2026-03-03 17:11:14 -0600

    cambio en config para conexion3

--- config.py
+++ config.py
@@ -1,25 +1,17 @@
 import os
 
 class Config:
-    # ==========================
-    # SEGURIDAD
-    # ==========================
     SECRET_KEY = os.getenv("SECRET_KEY", "dev_key_local")
 
-    # ==========================
-    # BASE DE DATOS
-    # ==========================
-    # Usa MYSQL_URL si está en Railway
+    # Obtén la URL de MySQL de Railway
     database_url = os.getenv("MYSQL_URL")
 
-    # Si existe MYSQL_URL, conviértelo a sqlalchemy+pymysql
     if database_url and database_url.startswith("mysql://"):
         database_url = database_url.replace("mysql://", "mysql+pymysql://", 1)
 
-    # Fallback local: MySQL en localhost
+    # SQLAlchemy usa la URL de Railway si existe
     SQLALCHEMY_DATABASE_URI = database_url or "mysql+pymysql://root:@localhost/sistema_aspirantes"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 
     # ==========================
     # CONFIGURACIÓN DE CORREO (GMAIL)
