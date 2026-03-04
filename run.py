5d39855da67337a108968e5c2b5d9d2498877b6a
fernandaAgustin <al222310475@gmail.com>
2026-03-03 23:54:22 -0600

    comprobacion de red2

--- run.py
+++ run.py
@@ -8,3 +8,11 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 5000))
     )
+
+    import socket
+
+try:
+    s = socket.create_connection(("smtp.gmail.com", 587), timeout=10)
+    print("Conexión exitosa")
+except Exception as e:
+    print("Error de conexión:", e)
